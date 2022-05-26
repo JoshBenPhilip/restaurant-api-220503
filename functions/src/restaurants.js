beb8c093f8c7f8e4dd2f37d6197d3798f9eafd40
JoshBenPhilip <joshben4c@gmail.com>
2022-05-26 16:13:42 -0400

    added rating function

--- functions/src/restaurants.js
+++ functions/src/restaurants.js
@@ -2,83 +2,133 @@ import connectDb from "../connectDb.js";
 
 export function getAllRestaurants(req, res) {
   const db = connectDb();
-  db.collection("restaurants").get()
-    .then(snapshot => {
-      const restaurantArray = snapshot.docs.map(doc => {
+  db.collection("restaurants")
+    .get()
+    .then((snapshot) => {
+      const restaurantArray = snapshot.docs.map((doc) => {
         let restaurant = doc.data();
         restaurant.id = doc.id;
         return restaurant;
       });
       res.send(restaurantArray);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 }
 
 export function getRestaurantById(req, res) {
   const { restaurantId } = req.params;
-  if(!restaurantId) {
-    res.status(401).send('Invalid request');
+  if (!restaurantId) {
+    res.status(401).send("Invalid request");
     return;
   }
   const db = connectDb();
-  db.collection("restaurants").doc(restaurantId).get()
-    .then(doc => {
+  db.collection("restaurants")
+    .doc(restaurantId)
+    .get()
+    .then((doc) => {
       let restaurant = doc.data();
       restaurant.id = doc.id;
       res.send(restaurant);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 }
 
 export function addRestaurant(req, res) {
-  if(!req.body) {
-    res.status(401).send('Invalid request');
+  if (!req.body) {
+    res.status(401).send("Invalid request");
     return;
   }
   const db = connectDb();
-  db.collection('restaurants').add(req.body)
-    .then(doc => {
-      res.send('Restaurant created ' + doc.id)
+  db.collection("restaurants")
+    .add(req.body)
+    .then((doc) => {
+      res.send("Restaurant created " + doc.id);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 }
 
-
-
 export function updateRestaurant(req, res) {
-  if(!req.params || !req.params.restaurantId || !req.body) {
-    res.status(401).send('Invalid request');
+  if (!req.params || !req.params.restaurantId || !req.body) {
+    res.status(401).send("Invalid request");
     return;
   }
   const { restaurantId } = req.params;
   const db = connectDb();
-  db.collection('restaurants').doc(restaurantId).update(req.body)
+  db.collection("restaurants")
+    .doc(restaurantId)
+    .update(req.body)
     .then(() => {
-      res.send('Restaurant updated.');
+      res.send("Restaurant updated.");
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 }
 
 export function deleteRestaurant(req, res) {
   const { restaurantId } = req.params;
-  if(!restaurantId) {
-    res.status(401).send('Invalid request');
+  if (!restaurantId) {
+    res.status(401).send("Invalid request");
     return;
   }
   const db = connectDb();
-  db.collection("restaurants").doc(restaurantId).delete()
+  db.collection("restaurants")
+    .doc(restaurantId)
+    .delete()
     .then(() => {
-      res.send('Restaurant deleted.');
+      res.send("Restaurant deleted.");
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err);
     });
 }
+
+export function updateRestaurantRating(req, res) {
+  const { restaurantId } = req.params;
+  if (
+    !req.body ||
+    !req.body.rating ||
+    req.body.rating > 5 ||
+    req.body.rating < 0
+  ) {
+    //problem
+    res.status(401).send("Invalid request");
+    return;
+  }
+  const { newRating } = req.body.rating;
+  const db = connectDb();
+  // get the restaurant (hitting Firestore)
+  db.collection("restaurants")
+    .doc(restaurantId)
+    .get()
+    .then((doc) => {
+      const { ratingList } = doc.data();
+      // do maths ( doing JS)
+      const newRatingList = ratingList
+        ? [...ratingList, newRating]
+        : [newRating];
+      const numRatings = newRatingList.length;
+      const rating = newRatingList.reduce((accum, elem) => accum + elem, 0) / numRatings;
+      const updatedData = { ratingList: newRatingList, numRatings, rating };
+      // save restaurant ( hitting Firestore)
+      db.collection('restaurants').doc(restaurantId).update(updatedData)
+      .then(() => getRestaurantById(req,res))
+
+
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+      return;
+    });
+  }
+  // option 1:
+  // return the updated restaurant
+
+  //options 2:
+  //return success true/false
